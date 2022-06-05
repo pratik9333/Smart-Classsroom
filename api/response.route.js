5c6db4629b21931bfe119806c8b35328da975c8e
Raghav Gupta <ragaws1674@gmail.com>
2022-06-06 01:42:00 +0530

    Add Complete support for Assignment Response controllers and routes
    Co-authored-by: Pratik Aswani <pratikaswani9333@gmail.com>

--- api/response.route.js
+++ api/response.route.js
@@ -1,22 +1,23 @@
 const { isAuth } = require("../middlewares/auth.middleware");
-const { getResponse, getAllResponses, createResponse, updateResponse, deleteResponse } = require("../services/response.service");
+const { getResponse, getAllResponses, createResponse, updateResponse, deleteResponse, 
+        preCreateResponseCheck,postCreateResponseCheck } = require("../services/response.service");
 
 const router = require("express").Router();
 
 // get a response for an assignment 
-router.get("/",isAuth,getResponse)
+router.get("/",isAuth,preCreateResponseCheck,postCreateResponseCheck,getResponse)
 
 // get all responses for an assignment
-router.get("/",isAuth,getAllResponses)
+router.get("/",isAuth,preCreateResponseCheck,postCreateResponseCheck,getAllResponses)
 
 // create a response for an assignment 
-router.post("/",isAuth,createResponse) 
+router.post("/",isAuth,preCreateResponseCheck,createResponse) 
 
 // update a response for an assignment
-router.put("/edit",isAuth,updateResponse)
+router.put("/",isAuth,preCreateResponseCheck,postCreateResponseCheck,updateResponse)
 
 // delete a response for an assignment
-router.delete("/delete",isAuth,deleteResponse)
+router.delete("/",isAuth,preCreateResponseCheck,postCreateResponseCheck,deleteResponse)
 
 module.exports = router;
 

6994d788406ecf614fb3f05a6f9609ab3b44282a
Pratik Aswani <pratikaswani9333@gmail.com>
2022-06-06 11:55:41 +0530

    UPDATE: changes in class controllers

--- middlewares/checkRole.middleware.js
+++ middlewares/checkRole.middleware.js
@@ -1,23 +1,19 @@
 const User = require("../models/user.model");
 
-exports.checkTeacherRole = async (req, res,next) => {
-    const loggedUser = await User.findByPk(req.userId);
+exports.checkTeacherRole = async (req, res, next) => {
+  const loggedUser = await User.findByPk(req.userId);
 
-    if (loggedUser.role !== "teacher") {
-        return res
-        .status(400)
-        .json({ error: "Unauthorized access" });
-    }
-    next();
+  if (loggedUser.role !== "teacher") {
+    return res.status(400).json({ error: "Unauthorized access" });
+  }
+  next();
 };
 
-exports.checkAdminRole = async (req, res,next) => {
-    const loggedUser = await User.findByPk(req.userId);
+exports.checkAdminRole = async (req, res, next) => {
+  const loggedUser = await User.findByPk(req.userId);
 
-    if (loggedUser.role !== "admin") {
-        return res
-        .status(400)
-        .json({ error: "Unauthorized access" });
-    }
-    next();
+  if (loggedUser.role !== "admin") {
+    return res.status(400).json({ error: "Unauthorized access" });
+  }
+  next();
 };
